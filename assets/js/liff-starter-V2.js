96f03a90d34f71a8bc180280d5dcfaf13b90e42b
PENDY <88583315+Suwidax@users.noreply.github.com>
2023-07-26 06:20:24 +0700

    Update liff-starter-V2.js

--- assets/js/liff-starter-V2.js
+++ assets/js/liff-starter-V2.js
@@ -2,7 +2,7 @@
 Author :
 - https://github.com/crash-override404
 */
-var liffId = "2000228366-lPokabO0";
+var liffId = "1584968724-qzg0aR95";
 var params = location.search.substring(1);
 var url = window.location.href;
 if (params) {
